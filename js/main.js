c4967b6ab5a82d815a41c60727aedc9509b7e27e
mikegibson66 <mikegibson66@byui.edu>
2021-04-24 13:19:47 -0600

    fix dynamic contents

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+        label: "Week 1 Index",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week 1: Story Editor",
+        url: "week1/story_editor.html"
+    }
+]
+
+function dynamicLnk() {
+    var finLink = '<ol>';
+
+    for (i = 0; i < links.length; i++) {
+        finLink += '<li><a href="' + links[i].url + '">' + links[i].label + '</a></li>';
+    }
+
+    finLink += '</ol>';
+
+    document.getElementById("directory").innerHTML = finLink;
+}
+
+window.onload = dynamicLnk;
