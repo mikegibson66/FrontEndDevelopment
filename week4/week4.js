6a697427eb59acb8be97ac503566e0f5de4830d2
mikegibson66 <mikegibson66@byui.edu>
2021-05-15 19:47:39 -0600

    Week four reading notes

--- week4/week4.js
+++ week4/week4.js
@@ -6,6 +6,38 @@ const links = [
     {
         label: "W04 - Team Activity",
         url: "Tic-Tac-Toe.html"
+    },
+    {
+        label: "JavaScript Notes/Examples - OOP in JavaScript",
+        url: "objectOrientedNotes.pdf"
+    },
+    {
+        label: "Event Listeners in JavaScript",
+        url: "search.html"
+    },
+    {
+        label: "More Event Listeners and Validations",
+        url: "hero.html"
+    },
+    {
+        label: "Novice to Ninja Notes - Forms",
+        url: "W04 - Forms.pdf"
+    },
+    {
+        label: "Novice to Ninja Notes - Object-Oriented Programming",
+        url: "W04 - Object-Oriented Programming.pdf"
+    },
+    {
+        label: "Novice to Ninja Notes - Modular Javascript",
+        url: "W04- Modular Javascript.pdf"
+    },
+    {
+        label: "Quiz Ninja - FORMS",
+        url: "W04-quizNinja.html"
+    },
+    {
+        label: "Quiz Ninja - Objects/Classes",
+        url: "W04b-quizNinja.html"
     }
     ]
 
