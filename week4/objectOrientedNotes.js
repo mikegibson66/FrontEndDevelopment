a5ceb19712db9effeb15b1de42ac7f75150452d8
mikegibson66 <mikegibson66@byui.edu>
2021-05-15 19:36:58 -0600

    Week four reading notes

--- week4/objectOrientedNotes.js
+++ week4/objectOrientedNotes.js
@@ -21,14 +21,24 @@ const blueDie = new Dice(5); // five sided die - can a five sided die be fair?
 * This was added in ES6
  */
 class Dice {
-    constructor(sides = 6) {
-        this.sides = sides;
+    constructor(sides=6){
+        Object.defineProperty(this, 'sides', {
+            get() {
+                return `This dice has ${sides} sides`;
+            },
+            set(value) {
+                if(value > 0) {
+                    sides = value;
+                    return sides;
+                } else {
+                    throw new Error('The number of sides must be positive');
+                }
+            }
+        });
+        this.roll = function() {
+            return Math.floor(sides * Math.random() + 1)
+        }
     }
-
-    roll() {
-        return Math.floor(this.sides * Math.random() + 1);
-    }
-
     static description() {  // static functions are not accessible from instances of the class
         return 'A way of choosing random numbers'; // only accessible from the class itself
         /* Dice.description(); */
@@ -75,4 +85,30 @@ class NinjaTurtle extends Turtle {
     attack() {
         return super.attack();
     }
+}
+
+/*
+* add isEven() and isOdd() to the Number wrapper object's prototype
+ */
+
+Number.prototype.isEven = () => this % 2 === 0;
+Number.prototype.isOdd = () => this % 2 === 1;
+
+/*
+* mixin: change the built-in function to make a deep copy, rather than a by reference copy
+ */
+function mixin(target,...objects) { // first parameter: is the object we are applying the mixin to
+    for (const object of objects) {
+        if(typeof object === 'object') {
+            for (const key of Object.keys(object)) {
+                if (typeof object[key] === 'object') {
+                    target[key] = Array.isArray(object[key]) ? [] : {};
+                    mixin(target[key],object[key]);
+                } else {
+                    Object.assign(target,object);
+                }
+            }
+        }
+    }
+    return target;
 }
