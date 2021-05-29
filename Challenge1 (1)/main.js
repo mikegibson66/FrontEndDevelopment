e4ad1bbfdcfc1cf5e63bf9aff9f7de384490feeb
mikegibson66 <mikegibson66@byui.edu>
2021-05-29 11:26:58 -0600

    Challenge 1 categorical views

--- Challenge1 (1)/main.js
+++ Challenge1 (1)/main.js
@@ -31,6 +31,58 @@ formItems.list.addEventListener('click', event => {
     }
 });
 
+formItems.selectActive.addEventListener('click', () => {
+    console.log('I clicked Active');
+    formItems.selectAll.classList.remove('selected');
+    formItems.selectDone.classList.remove('selected');
+    formItems.selectActive.classList.add('selected');
+
+    const showElements = document.getElementsByClassName('active');
+    let lenOfEl = showElements.length;
+    for(let i = 0; i < lenOfEl; i++) {
+        showElements[i].style.display = 'initial';
+    }
+
+    const hideElements = document.getElementsByClassName('done');
+    lenOfEl = hideElements.length;
+    for(let i = 0; i < lenOfEl; i++) {
+        hideElements[i].style.display = 'none';
+    }
+
+});
+formItems.selectDone.addEventListener('click', () => {
+    console.log('I clicked done');
+    formItems.selectAll.classList.remove('selected');
+    formItems.selectActive.classList.remove('selected');
+    formItems.selectDone.classList.add('selected');
+
+    const showElements = document.getElementsByClassName('done');
+    let lenOfEl = showElements.length;
+    for(let i = 0; i < lenOfEl; i++) {
+        showElements[i].style.display = 'initial';
+    }
+
+    const hideElements = document.getElementsByClassName('active');
+    lenOfEl = hideElements.length;
+    for(let i = 0; i < lenOfEl; i++) {
+        hideElements[i].style.display = 'none';
+    }
+
+});
+formItems.selectAll.addEventListener('click', () => {
+    console.log('I clicked all');
+    formItems.selectDone.classList.remove('selected');
+    formItems.selectActive.classList.remove('selected');
+    formItems.selectAll.classList.add('selected');
+
+    const showElements = document.getElementsByClassName('toDoText');
+    const lenOfEl = showElements.length;
+
+    for(let i = 0; i < lenOfEl; i++) {
+        showElements[i].style.display = 'initial';
+    }
+});
+
 function addTask() {
     const input = document.querySelector('.todo-input');
     const text = input.value.trim();
@@ -40,5 +92,4 @@ function addTask() {
         input.focus();
     }
 }
-
 
