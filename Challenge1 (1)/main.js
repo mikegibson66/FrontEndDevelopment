5b90bf4a8b52484cba09d349c97ed2c4c876428d
mikegibson66 <mikegibson66@byui.edu>
2021-05-29 00:20:58 -0600

    Challenge 1 update

--- Challenge1 (1)/main.js
+++ Challenge1 (1)/main.js
@@ -1,1 +1,44 @@
-import Todo from 'todo.js';
+import Todo from './todo.js';
+
+const goalList = new Todo;
+
+const formItems = {
+    selectAll: document.getElementById('all'),
+    selectActive: document.getElementById('active'),
+    selectDone: document.getElementById('done'),
+    newGoal: document.getElementById('new-goal'),
+    add: document.getElementById('add'),
+    form: document.querySelector('.enterElement'),
+    list: document.getElementById('list')
+}
+
+
+formItems.form.addEventListener('submit', event => {
+    event.preventDefault();
+    addTask();
+});
+
+formItems.add.addEventListener('click', () => addTask);
+
+formItems.list.addEventListener('click', event => {
+    if(event.target.classList.contains('js-tick')) {
+        const itemKey = event.target.id;
+        goalList.toggleDone(itemKey);
+    }
+    if(event.target.classList.contains('trash')) {
+        const itemKey = event.target.id;
+        goalList.deleteTask(itemKey);
+    }
+});
+
+function addTask() {
+    const input = document.querySelector('.todo-input');
+    const text = input.value.trim();
+    if (text !== '') {
+        goalList.addTask(text);
+        input.value = '';
+        input.focus();
+    }
+}
+
+
--- Challenge1 (1)/todo.js
+++ Challenge1 (1)/todo.js
@@ -5,18 +5,71 @@ class Todo {
         const todoItem = {
             text,
             checked: false,
-            id: Date.now()
+            id: Date.now(),
+            deleted: false
         };
 
         this.todoTasks.push(todoItem);
-        console.log(this.todoTasks)
+        console.log(this.todoTasks);
+        this.renderTodo(todoItem);
     }
 
-    get() {
-        return this.todoTasks;
+    renderTodo(todoItem) {
+        // select the list element in the DOM
+        const list = document.getElementById('list');
+        const item = document.querySelector(`[data-key = '${todoItem.id}']`);
+
+        // to remove deleted elements from list
+        if(todoItem.deleted) {
+            item.remove();
+            return
+        }
+
+        const isChecked = todoItem.checked ? 'toDoComplete' : '';
+        const checked = todoItem.checked ? 'checked' : '';
+
+        const node = document.createElement('div');
+        node.setAttribute('class', `toDoText`);
+        node.setAttribute('data-key', todoItem.id);
+
+        node.innerHTML = `
+        <div class='listItem'>
+        <input id="${todoItem.id}" type="checkbox" class="js-tick" ${checked}>
+        <div class="toDoText ${isChecked}">${todoItem.text}</div>
+        <div class="trash">🗑</div>
+        </div>
+        `;
+
+        if (item) {
+            // if item exists, replace
+            list.replaceChild(node, item);
+        } else {
+            // if it doesn't, append to the list
+            list.append(node);
+        }
     }
 
-    set(todoTasks) {
-        this.todoTasks = todoTasks;
+    toggleDone(key) {
+        const index = this.todoTasks.findIndex(item => item.id === Number(key));
+        this.todoTasks[index].checked = !this.todoTasks[index].checked;
+        this.renderTodo(this.todoTasks[index]);
     }
+
+
+    deleteTask(key) {
+        // find object in the todoTasks array
+        const index = this.todoTasks.findIndex(item => item.id === Number(key));
+
+        // create a new object with the properties of the object to be deleted
+        this.todoTasks[index].deleted = true;
+        const todo = {...this.todoTasks[index]};
+
+        // remove the object to be deleted from the array by filtering it out
+        this.todoTasks = this.todoTasks.filter(todoItem => todoItem.id !== Number(key));
+        console.log(this.todoTasks);
+        this.renderTodo(todo);
+    }
+
 }
+
+export default Todo;
